9f51117bf047b3bf8cc92d6a3c39c761aa0e03b9
Prasanthkumar07 <153902633+Prasanthkumar07@users.noreply.github.com>
2024-02-25 10:12:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,101 @@ app = Flask(__name__)
 def home():
     return render_template("intex.html")
 
+@app.route('/')
+def home():
+    return render_template("about.html")
 
+@app.route('/')
+def home():
+    return render_template("aerators.html")
+    
+@app.route('/')
+def home():
+    return render_template("blowers.html")
 
+@app.route('/')
+def home():
+    return render_template("carpet.html")
 
+@app.route('/')
+def home():
+    return render_template("compactors.html")
+
+@app.route('/')
+def home():
+    return render_template("concrete.html")
+
+@app.route('/')
+def home():
+    return render_template("contact.html")
+
+@app.route('/')
+def home():
+    return render_template("coredrill.html")
+
+@app.route('/')
+def home():
+    return render_template("dc.html")
+
+@app.route('/')
+def home():
+    return render_template("demolition.html")
+
+@app.route('/')
+def home():
+    return render_template("drain.html")
+
+@app.route('/')
+def home():
+    return render_template("drainclean.html")
+
+@app.route('/')
+def home():
+    return render_template("floor.html")
+
+@app.route('/')
+def home():
+    return render_template("flr.html")
+
+@app.route('/')
+def home():
+    return render_template("grinder.html")
+
+@app.route('/')
+def home():
+    return render_template("largerent.html")
+
+@app.route('/')
+def home():
+    return render_template("login.html")
+
+@app.route('/')
+def home():
+    return render_template("paint.html")
+
+@app.route('/')
+def home():
+    return render_template("paints.html")
+
+@app.route('/')
+def home():
+    return render_template("pressure.html")
+
+@app.route('/')
+def home():
+    return render_template("rent.html")
+
+@app.route('/')
+def home():
+    return render_template("rentform.html")
+
+@app.route('/')
+def home():
+    return render_template("sales.html")
+
+@app.route('/')
+def home():
+    return render_template("signup.html")
+     
 if __name__ == '__main__':
     app.run(debug=True)
